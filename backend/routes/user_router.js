7495a441ea83a0093dac3c0e8cb6d652751cbd26
ashtavinayaksh <ashtavinayaksharma555@gmail.com>
2021-09-01 11:04:28 +0530

    initial

---
+++ backend/routes/user_router.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var userss = require('../models/user_model');
+
+router.get("/:id?", function (req, res) {
+    //Pagination For number Of receords on page
+    if (req.params.id) {
+        //Case For Counting Number OF Users
+        userss.find({})
+        .count()
+        .then(data => {
+            res.status(200).send({
+                "cnt" : data
+            })
+        })
+        .catch(err => {
+           res.status(400).send({
+               "err" : err
+           })
+        })
+    }
+})
+
+router.post("/",function(req,res){
+    const pagination = req.body.pagination ? parseInt(req.body.pagination) : 20;
+    //PageNumber From which Page to Start 
+    const pageNumber = req.body.page ? parseInt(req.body.page) : 1;
+    userss.find({})
+        //skip takes argument to skip number of entries 
+        .sort({"id" : 1})
+        .skip((pageNumber - 1) * pagination)
+        //limit is number of Records we want to display
+        .limit(pagination)
+        .then(data => {
+            res.status(200).send({
+                "users": data
+            })
+        })
+        .catch(err => {
+            res.status(400).send({
+                "err": err
+            })
+        })
+})
+
+module.exports = router;
---
+++ src/App.js
@@ -0,0 +1,155 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import PaginationPage from "./Pagination/index";
+import { Table } from 'reactstrap';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [],
+      currentPage: 1,
+      totalUsers: 0
+    }
+  };
+  nextpage = (pageNumber) => {
+    this.setState({
+      currentPage: pageNumber,
+      users: []
+    })
+    this.getUsers(pageNumber);
+  }
+  tenChange = (pageNumber, isposOrneg) => {
+    var finalPage;
+    if (isposOrneg > 0) //+10 clicked
+      finalPage = pageNumber + 10;
+    else //-10 clicked
+      finalPage = pageNumber - 10;
+    this.setState({
+      currentPage: finalPage,
+      users: [],
+    })
+    this.getUsers(finalPage);
+  }
+
+  hundreadChange = (pageNumber, isposOrneg) => {
+    var finalPage;
+    if (isposOrneg > 0) //+100 clicked
+      finalPage = pageNumber + 100
+    else  //-100 Clicked
+      finalPage = pageNumber - 100
+    this.setState({
+      currentPage: finalPage,
+      users: [],
+    })
+    this.getUsers(finalPage);
+
+  }
+  dataRequest = (URL, methodType, params) => {
+    return fetch(URL, {
+      method: methodType,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
+    })
+      .then(data => {
+        return data.json()
+      })
+      .catch(err => {
+        return err
+      })
+  }
+  getUsers = (currentPage) => {
+    console.log("Rendering Inside GetUsers")
+    const queryParams = {};
+    queryParams["page"] = currentPage; //Page Number
+    queryParams["pagination"] = 8; //Number Of records on Page
+    this.dataRequest('http://localhost:5000/getUsers', 'POST', queryParams)
+      .then(data => {
+        console.log("Data FEtched ", data)
+        this.setState({
+          users: data.users
+        })
+      })
+      .catch(err => {
+        console.log("Error In Fetching Users ", err)
+      })
+  }
+  getUsersCount = () => {
+    //Passing /1 as Backend Uses same query so if argument then it will return count
+    this.dataRequest('http://localhost:5000/getUsers/1', 'GET')
+      .then(data => {
+        this.setState({
+          totalUsers: data.cnt
+        }, //call is for first page records only
+          () => this.getUsers(this.state.currentPage))
+      })
+  }
+  componentDidMount() {
+    this.getUsersCount()
+  }
+  render() {
+    let numberOfPages = 0;
+    if (this.state.totalUsers % 8 === 0)
+      numberOfPages = Math.floor(this.state.totalUsers / 8);
+    else
+      numberOfPages = Math.floor(this.state.totalUsers / 8) + 1;
+    return (
+
+      <div className="container" style={{marginLeft : "10%",width:"70%",marginTop : "3%"}}>
+        <div>
+          {
+            this.state.users.length > 0 ?
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th>id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email </th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  {
+                    this.state.users.map((data) => {
+                      return (
+                        <tr>
+                          <th scope="row">{data.id}</th>
+                          <td>{data.first_name}</td>
+                          <td>{data.last_name}</td>
+                          <td>{data.email}</td>
+                        </tr>
+                      )
+                    })
+                  }
+
+                </tbody>
+              </Table>
+              :
+              "No Data"
+          }
+        </div>
+        {
+          this.state.totalUsers > 8 &&
+          <PaginationPage
+            pages={numberOfPages}
+            nextPage={this.nextpage}
+            currentPage={this.state.currentPage}
+            hundreadChange={this.hundreadChange}
+            tenChange={this.tenChange}
+          >
+          </PaginationPage>
+       }
+      
+      </div >
+     
+    );
+  }
+}
+
+export default App;
